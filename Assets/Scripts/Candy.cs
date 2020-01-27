5c16a15276480dab3909ae82ab41dab522b94152
steve97harris <34012174+steve97harris@users.noreply.github.com>
2020-01-27 11:00:50 +0000

    candy spawn

---
+++ Assets/Scripts/Candy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Candy : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            // Increment score
+            Destroy(gameObject);
+        }
+        
+        else if (collider.gameObject.tag == "Boundary")
+        {
+            // Decrease lives
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/CandySpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandySpawner : MonoBehaviour
+{
+    [SerializeField]
+    float maxX;
+
+    public GameObject[] candies;
+    
+    void Start()
+    {
+        SpawnCandy();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void SpawnCandy()
+    {
+        int randomCandy = Random.Range(0, candies.Length);
+
+        Instantiate(candies[randomCandy], transform.position, transform.rotation);
+    }
+}
